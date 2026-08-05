ae12143c1122588879b1829fd4c9db7c2c439cb1
Invalid07 <vu1f2122127@pvppcoe.ac.in>
2026-08-05 13:21:09 +0530

    office : setting project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vikas yadav",
+    author_email="viikkas108@gmail.com",
+    packages=find_packages()
+)
