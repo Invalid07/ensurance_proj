007d8c197b3dc2d669cfa7f01694b81d6c070b33
Invalid07 <vu1f2122127@pvppcoe.ac.in>
2026-08-19 13:36:15 +0530

    office :till model pusher

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,5 +1,8 @@
 import os
 from datetime import date
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # For MongoDB connection
 DATABASE_NAME = "Proj1"
@@ -23,6 +26,7 @@ SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+endpoint_url="https://s3.us-east-1.amazonaws.com" 
 REGION_NAME = "us-east-1"
 
 
@@ -67,7 +71,7 @@ MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
 MODEL Evaluation related constants
 """
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "my-model-mlopsproj"
+MODEL_BUCKET_NAME = "my-model-mlops-proj-1-410311315424-us-east-1-an"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
